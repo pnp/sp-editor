4492bfc77435147d630272b4c774948c4a0573a0
tomitavela <tomi.tavela@valosolutions.com>
2023-02-14 09:20:45 +0200

    refactored for simplicity

--- src/pages/spshooter/chrome/chrome-actions.ts
+++ src/pages/spshooter/chrome/chrome-actions.ts
@@ -3,144 +3,18 @@ import * as rootActions from '../../../store/home/actions'
 import { HomeActions, MessageBarColors } from '../../../store/home/types'
 import * as actions from '../../../store/spshoot/actions'
 import { ISPShootPayload, SPShootActions } from '../../../store/spshoot/types'
-import { exescript } from '../../../utilities/chromecommon'
-import { getPnpjsPath, getSystemjsPath } from '../../../utilities/utilities'
-import { getContext } from './getContext'
+import { shoot } from './shoot'
 
 export async function runRestCall(dispatch: Dispatch<SPShootActions | HomeActions>, payload: ISPShootPayload) {
 
   dispatch(rootActions.setLoading(true));
 
-  const executeScript = (payload: any, extPath: string) => {
-
-    const loadScript = (src: string) => {
-      return new Promise<void>(function (resolve) {
-        const s = document.createElement('script');
-        s.src = src;
-        s.onload = () => resolve();
-        (document.head || document.documentElement).appendChild(s);
-      });
-    }
-
-    const shoot = (payload: any) => {
-
-      /* import pnp */
-      return Promise.all([
-        (window as any).SystemJS.import(extPath + 'bundles/sp.es5.umd.bundle.js'),
-        (window as any).SystemJS.import(extPath + 'bundles/logging.es5.umd.bundle.js'),
-        (window as any).SystemJS.import(extPath + 'bundles/queryable.es5.umd.bundle.js')])
-        .then(modules => {
-
-          /*** map modules ***/
-          var pnpsp = modules[0];
-          var pnplogging = modules[1];
-          var pnpqueryable = modules[2];
-
-          /*** custom httpHandler ***/
-          const rawFetchHandler = () => {
-            return instance => {
-              instance.using(pnpsp.DefaultHeaders(), pnpsp.DefaultInit(), pnpqueryable.BrowserFetchWithRetry(), pnpqueryable.DefaultParse(), pnpsp.RequestDigest());
-              instance.on.pre.prepend((url, init, result) => {
-                if (url.indexOf('###') > -1) {
-                  return [url.substring(url.indexOf("###") + 3), init, result];
-                }
-                return [url, init, result];
-              });
-              return instance;
-            };
-          }
-
-          /***  init pnpjs ***/
-          const sp = pnpsp.spfi().using(pnpsp.SPBrowser({
-            baseUrl: (window as any)._spPageContextInfo.webAbsoluteUrl
-          })).using(rawFetchHandler()).using(pnpqueryable.InjectHeaders({
-            "Accept": "application/json; odata=verbose",
-            "Cache-Control": "no-cache",
-            "X-ClientService-ClientTag": "SPEDITOR"
-          }));
-
-          /*** clear previous log listeners ***/
-          pnplogging.Logger.clearSubscribers();
-
-          /*** setup log listener ***/
-          const listener = pnplogging.FunctionListener(entry => {
-            entry.data.response.clone().json().then(error => {
-              return {
-                success: false,
-                result: null,
-                errorMessage: error.error.message.value,
-                source: 'chrome-sp-editor'
-              }
-            });
-          });
-          pnplogging.Logger.subscribe(listener);
-
-          /*** check that headers valid object ***/
-          try {
-            JSON.parse(payload.headers);
-          } catch (ex) {
-            return {
-              success: false,
-              result: null,
-              errorMessage: 'Could not parse request headers!: ' + payload.headers,
-              source: 'chrome-sp-editor'
-            }
-          }
-
-          /*** execute request, add ### to trigger custom httpHandler ***/
-          return pnpsp.spPost(pnpsp.Web(sp.web, "###" + payload.path), {
-            headers: JSON.parse(payload.headers),
-            method: payload.method,
-            body: payload.method === 'GET' ? null : payload.body
-          }).then(json => {
-            if (json.status === 204) {
-              return json
-            } else {
-              if (json && json['odata.error']) {
-                const errori = json['odata.error'].message.value;
-                return {
-                  success: false,
-                  result: null,
-                  errorMessage: errori.length > 300 ? errori.substring(0, 299) : errori,
-                  source: 'chrome-sp-editor'
-                }
-              } else {
-                return json
-              }
-            }
-          }
-          ).catch(error => {
-            var errorMessage = error.message;
-            if (error !== null && error !== void 0 && error.isHttpRequestError) {
-              // we can read the json from the response
-              const json = error.response.json().then(json => {
-                // if we have a value property we can show it
-                errorMessage = typeof json["odata.error"] === "object" ? json["odata.error"].message.value : error.message;
-              });
-            } else {
-              // not an HttpRequestError so we just log message
-              console.log(error);
-            }
-            return {
-              success: false,
-              result: null,
-              errorMessage: errorMessage,
-              source: 'chrome-sp-editor'
-            }
-          });
-        });
-    }
-
-    return loadScript(extPath + 'bundles/system.js').then(() => shoot(payload))
-  }
-
   chrome.tabs.query({ currentWindow: true, active: true }, (tabs: any) => {
-
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
       world: 'MAIN',
       args: [payload, chrome.runtime.getURL('')],
-      func: executeScript,
+      func: shoot,
     }).then(injectionResults => {
       if (injectionResults[0].result) {
         const res = injectionResults[0].result
@@ -150,12 +24,8 @@ export async function runRestCall(dispatch: Dispatch<SPShootActions | HomeAction
             message: res.errorMessage,
             color: MessageBarColors.danger,
           }))
-          dispatch(actions.setResults(res))
-        } else {
-          console.log(res) // log the result for testing
-          dispatch(actions.setResults(res))
-          // hide loading component
         }
+        dispatch(actions.setResults(res))
         dispatch(rootActions.setLoading(false))
       }
     });
--- src/pages/spshooter/chrome/shoot.ts
+++ src/pages/spshooter/chrome/shoot.ts
@@ -1,166 +1,122 @@
 import * as SP from '@pnp/sp/presets/all'
 import * as Logging from '@pnp/logging'
 import * as Queryable from '@pnp/queryable'
-import { ISPShootPayload } from '../../../store/spshoot/types'
-import { HttpRequestError } from '@pnp/queryable'
 
-// we cannot use async methods, they do not work correctly when running 'npm run build',
-// async methods works when running 'npm run watch'
-export function shoot(...args: any) {
-  /* get parameters */
-  const params = args
-  const functionName = params[0].name
-  const payload: ISPShootPayload = JSON.parse(
-    decodeURIComponent(params[1]).replace(/%27/g, "'"),
-  );
+export const shoot = (payload: any, extPath: string) => {
 
-  /* import pnp */
-  type libTypes = [Promise<typeof SP>, Promise<typeof Logging>, Promise<typeof Queryable>];
+  /*** load systemjs ***/
+  return new Promise<any>((resolve) => {
+    const s = document.createElement('script');
+    s.src = extPath + 'bundles/system.js';
+    (document.head || document.documentElement).appendChild(s);
+    s.onload = () => {
 
-  Promise.all<libTypes>([
-    (window as any).SystemJS.import((window as any).mod_sp),
-    (window as any).SystemJS.import((window as any).mod_logging),
-    (window as any).SystemJS.import((window as any).mod_queryable)
-  ]).then((modules) => {
+      type libTypes = [Promise<typeof SP>, Promise<typeof Logging>, Promise<typeof Queryable>];
+      /*** load pnpjs modules ***/
+      return Promise.all<libTypes>([
+        (window as any).SystemJS.import(extPath + 'bundles/sp.es5.umd.bundle.js'),
+        (window as any).SystemJS.import(extPath + 'bundles/logging.es5.umd.bundle.js'),
+        (window as any).SystemJS.import(extPath + 'bundles/queryable.es5.umd.bundle.js')])
+        .then(modules => {
 
-    var pnpsp = modules[0]
-    var pnplogging = modules[1]
-    var pnpqueryable = modules[2]
+          /*** map modules ***/
+          var pnpsp = modules[0];
+          var pnplogging = modules[1];
+          var pnpqueryable = modules[2];
 
-    function rawFetchHandler() {
-      return (instance) => {
-        instance.using(
-          pnpsp.DefaultHeaders(),
-          pnpsp.DefaultInit(),
-          pnpqueryable.BrowserFetchWithRetry(),
-          pnpqueryable.DefaultParse(),
-          pnpsp.RequestDigest());
-
-        instance.on.pre.prepend((url, init, result) => {
-
-          if (url.indexOf('###') > -1) {
-            return [url.substring(url.indexOf("###") + 3), init, result];
+          /*** custom httpHandler ***/
+          const rawFetchHandler = () => {
+            return instance => {
+              instance.using(pnpsp.DefaultHeaders(), pnpsp.DefaultInit(), pnpqueryable.BrowserFetchWithRetry(), pnpqueryable.DefaultParse(), pnpsp.RequestDigest());
+              instance.on.pre.prepend((url, init, result) => {
+                if (url.indexOf('###') > -1) {
+                  return [url.substring(url.indexOf("###") + 3), init, result];
+                }
+                return [url, init, result];
+              });
+              return instance;
+            };
           }
 
-          return [url, init, result];
-        });
-        return instance;
-      };
-    }
+          /***  init pnpjs ***/
+          const sp = pnpsp.spfi().using(pnpsp.SPBrowser({
+            baseUrl: (window as any)._spPageContextInfo.webAbsoluteUrl
+          })).using(rawFetchHandler()).using(pnpqueryable.InjectHeaders({
+            "Accept": "application/json; odata=verbose",
+            "Cache-Control": "no-cache",
+            "X-ClientService-ClientTag": "SPEDITOR"
+          }));
 
-    const sp = pnpsp.spfi().using(pnpsp.SPBrowser({ baseUrl: (window as any)._spPageContextInfo.webAbsoluteUrl }))
-      .using(rawFetchHandler())
-      .using(pnpqueryable.InjectHeaders({
-        "Accept": "application/json; odata=verbose",
-        "Cache-Control": "no-cache",
-        "X-ClientService-ClientTag": "SPEDITOR"
-      }));
+          /*** clear previous log listeners ***/
+          pnplogging.Logger.clearSubscribers();
 
-    /*** clear previous log listeners ***/
-    pnplogging.Logger.clearSubscribers()
-    /*** setup log listener ***/
-    const listener = pnplogging.FunctionListener((entry) => {
-      entry.data.response
-        .clone()
-        .json()
-        .then((error: any) => {
-          window.postMessage(
-            JSON.stringify({
-              function: functionName,
-              success: false,
-              result: null,
-              errorMessage: error.error.message.value,
-              source: 'chrome-sp-editor',
-            }),
-            '*',
-          )
-        })
-    })
-    pnplogging.Logger.subscribe(listener)
-    /* *** */
-
-    const postMessage = (actions: any) => {
-      window.postMessage(
-        JSON.stringify({
-          function: functionName,
-          success: true,
-          result: actions,
-          errorMessage: '',
-          source: 'chrome-sp-editor',
-        }),
-        '*',
-      )
-    }
-
-    try {
-      JSON.parse(payload.headers)
-    } catch (ex) {
-      window.postMessage(
-        JSON.stringify({
-          function: functionName,
-          success: false,
-          result: null,
-          errorMessage:
-            'Could not parse request headers!: ' + payload.headers,
-          source: 'chrome-sp-editor',
-        }),
-        '*',
-      )
-      return
-    }
-
-    pnpsp.spPost(pnpsp.Web(sp.web, "###" + payload.path), {
-      headers: JSON.parse(payload.headers),
-      method: payload.method,
-      body: payload.method === 'GET' ? null : payload.body,
-    })
-      .then((json) => {
-        if (json.status === 204) {
-          postMessage(JSON.stringify(json))
-        } else {
-          if (json && json['odata.error']) {
-            const errori = json['odata.error'].message.value
-            window.postMessage(
-              JSON.stringify({
-                function: functionName,
+          /*** setup log listener ***/
+          const listener = pnplogging.FunctionListener(entry => {
+            entry.data.response.clone().json().then(error => {
+              return {
                 success: false,
                 result: null,
-                errorMessage: errori.length > 300 ? errori.substring(0, 299) : errori,
-                source: 'chrome-sp-editor',
-              }),
-              '*',
-            )
-          } else {
-            postMessage(json)
+                errorMessage: error.error.message.value,
+                source: 'chrome-sp-editor'
+              }
+            });
+          });
+          pnplogging.Logger.subscribe(listener);
+
+          /*** check that headers valid object ***/
+          try {
+            JSON.parse(payload.headers);
+          } catch (ex) {
+            resolve({
+              success: false,
+              result: null,
+              errorMessage: 'Could not parse request headers!: ' + payload.headers,
+              source: 'chrome-sp-editor'
+            })
           }
-        }
-      })
-      .catch((error) => {
-        var errorMessage = error.message;
-        if (error?.isHttpRequestError) {
 
-          // we can read the json from the response
-          const json = (<HttpRequestError>error).response.json().then(json => {
-
-            // if we have a value property we can show it
-            errorMessage = typeof json["odata.error"] === "object" ? json["odata.error"].message.value : error.message
-          })
-        } else {
-          // not an HttpRequestError so we just log message
-          console.log(error);
-        }
-
-        window.postMessage(
-          JSON.stringify({
-            function: functionName,
-            success: false,
-            result: null,
-            errorMessage: errorMessage,
-            source: 'chrome-sp-editor',
-          }),
-          '*',
-        )
-      })
-  },
-  )
+          /*** execute request, add ### to trigger custom httpHandler ***/
+          return pnpsp.spPost(pnpsp.Web(sp.web, "###" + payload.path), {
+            headers: JSON.parse(payload.headers),
+            method: payload.method,
+            body: payload.method === 'GET' ? null : payload.body
+          }).then(json => {
+            if (json.status === 204) {
+              resolve(json);
+            } else {
+              if (json && json['odata.error']) {
+                const errori = json['odata.error'].message.value;
+                resolve({
+                  success: false,
+                  result: null,
+                  errorMessage: errori.length > 300 ? errori.substring(0, 299) : errori,
+                  source: 'chrome-sp-editor'
+                })
+              } else {
+                resolve(json)
+              }
+            }
+          }
+          ).catch(error => {
+            var errorMessage = error.message;
+            if (error !== null && error !== void 0 && error.isHttpRequestError) {
+              // we can read the json from the response
+              const json = error.response.json().then(json => {
+                // if we have a value property we can show it
+                errorMessage = typeof json["odata.error"] === "object" ? json["odata.error"].message.value : error.message;
+              });
+            } else {
+              // not an HttpRequestError so we just log message
+              console.log(error);
+            }
+            resolve({
+              success: false,
+              result: null,
+              errorMessage: errorMessage,
+              source: 'chrome-sp-editor'
+            })
+          });
+        });
+    }
+  });
 }
