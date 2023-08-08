218bec575f04e37988a2fdabfdf4726fa7d161a3
Tomi Tavela <tomi.tavela@gmail.com>
2023-08-08 13:54:24 +0300

    search init

---
+++ src/pages/search/components/commands.tsx
@@ -0,0 +1,15 @@
+import { ActionButton, DefaultButton, Dialog, DialogFooter, DialogType, Dropdown, IStackStyles, PrimaryButton, Stack, TextField } from '@fluentui/react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { IRootState } from '../../../store'
+
+const SearchCommands = () => {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default SearchCommands
---
+++ src/pages/search/components/searchresults.tsx
@@ -0,0 +1,15 @@
+import { Pivot, PivotItem } from '@fluentui/react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { IRootState } from '../../../store'
+
+const SearchResults = () => {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default SearchResults
