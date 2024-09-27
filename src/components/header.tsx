2629b3c74180955166b619371f9e399924d5e544
Tomi Tavela <tomi.tavela@gmail.com>
2024-09-27 16:21:05 +0300

    buy me a coffee

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { DirectionalHint, FontIcon, mergeStyles, TeachingBubble } from '@fluentui/react';
 import { IonButtons, IonHeader, IonMenuButton, IonMenuToggle, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import coffee from './default-yellow.png';
 
 const Header = ({ title, showOnLoad, headline, content }: HeaderProps) => {
   const [showInfo, setShowInfo] = React.useState(showOnLoad);
@@ -20,6 +21,9 @@ const Header = ({ title, showOnLoad, headline, content }: HeaderProps) => {
             <IonMenuButton />
           </IonMenuToggle>{' '}
           <IonTitle>{title}</IonTitle>
+          <a href="https://buymeacoffee.com/speditor" target="_blank" rel="noopener noreferrer">
+            <img src={coffee} alt="coffee" style={{ marginRight: '10px', height: '40px' }} />
+          </a>{' '}
           {headline && content && (
             <div style={{ marginRight: '20px', display: 'flex', alignItems: 'center' }}>
               <FontIcon
