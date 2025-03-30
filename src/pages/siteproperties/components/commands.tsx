452939581a3ce52d4942647faec8b514b0fb03c3
Prateek Chaudhary <prateek.chaudhary@contentandcloud.com>
2025-03-30 21:21:25 +0100

    Picker props

--- src/pages/siteproperties/components/commands.tsx
+++ src/pages/siteproperties/components/commands.tsx
@@ -51,6 +51,7 @@ const SitePropertiesCommands = () => {
                       }));
                     });
                   }}
+                  pickerCalloutProps={{ doNotLayer: true }}
                 />
               </Stack>
             </>
