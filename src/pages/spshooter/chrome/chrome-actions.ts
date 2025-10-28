3ab02764dad0a94069fe00bad49bdfba8329a912
Tomi Tavela <tomi.tavela@gmail.com>
2025-10-28 16:36:25 +0200

    SP Shooter to firefox

--- src/pages/spshooter/chrome/chrome-actions.ts
+++ src/pages/spshooter/chrome/chrome-actions.ts
@@ -3,59 +3,64 @@ import * as rootActions from '../../../store/home/actions'
 import { HomeActions, MessageBarColors } from '../../../store/home/types'
 import * as actions from '../../../store/spshoot/actions'
 import { ISPShootPayload, SPShootActions } from '../../../store/spshoot/types'
+import { executeScript } from '../../../utilities/script-injection'
 import { shoot } from './shoot'
 
 export async function runRestCall(dispatch: Dispatch<SPShootActions | HomeActions>, payload: ISPShootPayload) {
-
   dispatch(rootActions.setLoading(true));
 
-  chrome.scripting.executeScript({
-    target: { tabId: chrome.devtools.inspectedWindow.tabId },
-    world: 'MAIN',
-    args: [payload, chrome.runtime.getURL('')],
-    func: shoot,
-  }).then(injectionResults => {
-    if (injectionResults[0].result) {
-      const res = injectionResults[0].result
-      if (res.success === false) {
-        dispatch(rootActions.setAppMessage({
-          showMessage: true,
-          message: res.errorMessage,
-          color: MessageBarColors.danger,
-        }))
-      }
-      dispatch(actions.setResults(res))
-      dispatch(rootActions.setLoading(false))
+  try {
+    const res = await executeScript('shoot', shoot, [payload, chrome.runtime.getURL('')]);
+    
+    if (res && res.success === false) {
+      dispatch(rootActions.setAppMessage({
+        showMessage: true,
+        message: res.errorMessage,
+        color: MessageBarColors.danger,
+      }));
     }
-  });
-
+    
+    dispatch(actions.setResults(res));
+    dispatch(rootActions.setLoading(false));
+  } catch (error) {
+    dispatch(rootActions.setLoading(false));
+    dispatch(rootActions.setAppMessage({
+      showMessage: true,
+      message: error instanceof Error ? error.message : 'Failed to execute REST call',
+      color: MessageBarColors.danger,
+    }));
+  }
 }
 
 export async function getContextInfo(dispatch: Dispatch<SPShootActions | HomeActions>) {
-
   dispatch(rootActions.setLoading(true));
 
-  chrome.scripting.executeScript({
-    target: { tabId: chrome.devtools.inspectedWindow.tabId },
-    world: 'MAIN',
-    func: () => {
+  try {
+    const getContextFunc = () => {
       return (window as any)._spPageContextInfo || ((window as any).moduleLoaderPromise ? (window as any).moduleLoaderPromise.then((e: any) => {
         return (window as any)._spPageContextInfo = e.context._pageContext._legacyPageContext;
       }) : null);
-    }
-  }).then(injectionResults => {
-    if (injectionResults[0].result) {
-      const res = injectionResults[0].result
-      dispatch(actions.setContext(res))
-      dispatch(rootActions.setLoading(false))
+    };
+
+    const res = await executeScript('getContextInfo', getContextFunc, []);
+    
+    if (res) {
+      dispatch(actions.setContext(res));
     } else {
       dispatch(rootActions.setAppMessage({
         showMessage: true,
         message: 'Could not get context info!',
         color: MessageBarColors.danger,
-      }))
+      }));
     }
-    dispatch(rootActions.setLoading(false))
-  });
-
+    
+    dispatch(rootActions.setLoading(false));
+  } catch (error) {
+    dispatch(rootActions.setLoading(false));
+    dispatch(rootActions.setAppMessage({
+      showMessage: true,
+      message: error instanceof Error ? error.message : 'Failed to get context info',
+      color: MessageBarColors.danger,
+    }));
+  }
 }
